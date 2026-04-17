6055d45b7ad6c52633356be9c7e053da89ab973c
Yuliia <50263234+Yuliia010@users.noreply.github.com>
2026-04-17 21:25:26 +0200

    reset pass edded

--- apps/users/services/emailMessages.py
+++ apps/users/services/emailMessages.py
@@ -4,3 +4,7 @@ class EmailMessages:
     CONFIRMED_SUCCESS = "Email successfully confirmed"
     RESEND_SUCCESS = "If the account exists and is not yet confirmed, the email has been sent"
     EMAIL_REQUIRED = "Email is required"
+
+    PASSWORD_RESET_SENT = "If the account exists, a password reset email has been sent"
+    PASSWORD_RESET_INVALID = "Invalid or expired password reset link"
+    PASSWORD_RESET_SUCCESS = "Password has been successfully changed"
