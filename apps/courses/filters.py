4b203bbe38572e9196d752ed857a90db0a3b505a
MiuselmianMV <m.maksim2603@icloud.com>
2026-05-01 19:31:56 +0300

    added search

--- apps/courses/filters.py
+++ apps/courses/filters.py
@@ -1,4 +1,5 @@
 import django_filters
+from django.db.models import Q
 
 from apps.courses.models import Course
 
@@ -8,7 +9,24 @@ class CourseFilter(django_filters.FilterSet):
         field_name="category__slug",
         lookup_expr="exact",
     )
+    search = django_filters.CharFilter(method="filter_search")
+
+    def filter_search(self, queryset, name, value):
+        query = value.strip()
+
+        if not query:
+            return queryset
+
+        return queryset.filter(
+            Q(title__icontains=query)
+            | Q(short_description__icontains=query)
+            | Q(full_description__icontains=query)
+            | Q(category__name__icontains=query)
+            | Q(tags__name__icontains=query)
+            | Q(teacher_profile__user__first_name__icontains=query)
+            | Q(teacher_profile__user__last_name__icontains=query)
+        ).distinct()
 
     class Meta:
         model = Course
-        fields = ["category"]
+        fields = ["category", "search"]
