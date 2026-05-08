5a5213debe678ef8d9122d35ffce7021f7fa64db
unknown xplt? <myrospolil@gmail.com>
2026-05-08 13:53:06 +0000

    config: extend access token lifetime to 24h in debug mode

--- config/settings.py
+++ config/settings.py
@@ -183,7 +183,7 @@ SPECTACULAR_SETTINGS = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
+    "ACCESS_TOKEN_LIFETIME": timedelta(hours=24) if DEBUG else timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
